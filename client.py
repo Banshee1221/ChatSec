8bfa4bc06cd0c8cd066615d3648dbd4eb075f58b
Eugene <banshee1221@gmail.com>
2015-03-20 17:13:28 +0200

    some auth changes

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
 __author__ = 'Eugene'
 
-import socket, random, pickle
-from AES import *
-from Crypto.Cipher import AES
+import socket
+import random
+import pickle
+import logging
+import sys
 from thread import *
+from Crypto.Cipher import AES
+from AES import *
+
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 
 class Client():
-
     clientID = 0
     sharedKey = 0
     IP = 0
@@ -15,49 +21,100 @@ class Client():
     cli_sock = None
     listenOnly = False
     listenSock = ''
-    others = ''
+    others = {}
+    listenIP = ''
+    listenPORT = ''
+    cipher = ''
 
     def __init__(self, ID, key, IP, port, listenOnly=False):
         self.clientID = ID
         self.sharedKey = key
         self.IP = IP
         self.port = port
-        connectTo = str(raw_input("Who: "))
         self.listenOnly = listenOnly
-        #self.authSrv(connectTo)
+        # self.authSrv(connectTo)
         # start_new_thread(self.authSrv, connectTo)
 
         # Binding own port and connecting to server
-        self.listenSock = socket.socket()
-        self.listenSock.bind(("", 0))
-        self.cli_sock = socket.socket()
+        self.cipher = AES.new(self.sharedKey)
+        start_new_thread(self.cliActListen, ())
+        print "Self-listening started"
+        self.cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.cli_sock.connect((self.IP, self.port))
+        print "Connecting to server with IP: " + str(self.IP) + ", PORT: " + str(self.port)
 
         self.identify()
-        start_new_thread(self.chatListener())
+        #start_new_thread(self.chatListener())
+
+    def menu(self):
+        print "Select one of:"
+        for all in self.others:
+            print all
+        choice = str(raw_input(":: "))
+        dictRet = self.others[choice]
+        logging.info("Chosen %s", str(dictRet))
+        self.authCli(choice, dictRet['IP'], dictRet['PORT'])
 
     def identify(self):
-        self.send(pickle.dumps([self.clientID, self.listenSock.getsockname()]))
+        id = self.clientID
+        idEnc = encrypt(id, self.cipher)
+        self.send(pickle.dumps((id, self.listenIP, self.listenPORT, idEnc)))
+        self.others = pickle.loads(self.cli_sock.recv(1024))
+        self.menu()
 
-
-    def authSrv(self, who):
+    def authSrv(self, who, encData):
         nonce = random.randrange(1, 100000000)
         expiry = 50000
-        packet = pickle.dumps([self.clientID, who, expiry, nonce])
+        packet = pickle.dumps([self.clientID, who, expiry, nonce, encData])
         self.send(packet)
         recvEnc = self.cli_sock.recv(4096)
-        print pickle.loads(recvEnc)
+        logging.info("Received data from CA server: %s", pickle.loads(recvEnc))
         decr = decrypt(pickle.loads(recvEnc), AES.new(self.sharedKey))
-        print decr
+        logging.info("Decoded information from the CA serv: %s", decr)
 
     def authCli(self, cli_id, cli_ip, cli_port):
-        pass
+        cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cli_sock.connect((cli_ip, cli_port))
+        logging.info("Connected to client %s", cli_id)
+        cli_sock.sendall(cli_id)
+        logging.info("Sending the ID to the client")
+        enc = cli_sock.recv(1024)
+        cli_sock.close()
+        logging.info("Info received from client: %s", enc.replace("\n", '').replace("\r", ''))
+        self.authSrv(cli_id, enc)
 
     def chatListener(self):
+        logging.info('Listening for info')
+        conn, addr = self.listenSock.accept()
+        logging.info('Received connection: %s, address: %s', conn, addr)
         while True:
-            self.listenSock.listen(10)
-            conn, addr = self.listenSock.accept()
-            print conn.recv(4096)
+            data = conn.recv(4096)
+            if not data:
+                break
+        conn.close()
+        return data
 
     def send(self, msg):
         self.cli_sock.sendall(msg)
+
+    def cliActListen(self):
+        self.listenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listenSock.bind(("127.0.0.1", 0))
+        temp = self.listenSock.getsockname()
+        print "Binding socket: " + str(temp)
+        self.listenIP = temp[0]
+        self.listenPORT = temp[1]
+        self.listenSock.listen(5)
+        conn, addr = self.listenSock.accept()
+        logging.info("Connected from %s", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            logging.info("Received data: %s", data)
+            if int(data) == self.clientID:
+                nonce = random.randrange(1, 100000000)
+                toSend = encrypt([data, nonce], self.cipher)
+                logging.info("Sending information [%s] back to client", toSend)
+                conn.sendall(toSend)
+        conn.close()
--- server.py
+++ server.py
@@ -2,18 +2,20 @@ import socket
 import sys
 from thread import *
 import pickle
-from AES import *
-from Crypto.Cipher import AES
-import random
 import logging
 import os
+
+from Crypto.Cipher import AES
+
+from AES import *
 
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 HOST = ''  # Symbolic name meaning all available interfaces
 PORT = 8888  # Arbitrary non-privileged port
 CLIENTS = {}
-KEYS = {1: '(e\xd0\t\xacn\xa8k}\xbe\x80s)>m\x83', 2: '({l\xa8\xee\x00\xf0\xe6b\xb8\n\x96\xb8\xcc\xd20', 3: '\xbaB\x80\x96\x84\x15*\x1b\x0e\xc9\xbb\xbdF~\x8a9'}
+KEYS = {1: '(e\xd0\t\xacn\xa8k}\xbe\x80s)>m\x83', 2: '({l\xa8\xee\x00\xf0\xe6b\xb8\n\x96\xb8\xcc\xd20',
+        3: '\xbaB\x80\x96\x84\x15*\x1b\x0e\xc9\xbb\xbdF~\x8a9'}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
@@ -45,18 +47,29 @@ def clientthread(conn):
             data = conn.recv(1024)
         except socket.error:
             logging.info("Client disconnected.")
+
             conn.close()
         if not data:
             break
 
-        print "Received: "+data.replace('\n', '').replace('\r', '')
+        print "Received: " + data.replace('\n', '').replace('\r', '')
         try:
             initAuth = pickle.loads(data)
-            if isinstance(initAuth, list):
+            if isinstance(initAuth, tuple):
                 if str(initAuth[0]) in CLIENTS:
-                    logging.info("User already in connected list.")
+                    logging.info("User already in connected list")
                 else:
-                    CLIENTS[str(initAuth[0])] = {'IP': initAuth[1][0], 'PORT': initAuth[1][0]}
+                    logging.info("Client incoming, checking validity")
+                    tempCiph = AES.new(KEYS[int(initAuth[0])])
+                    checker = decrypt(initAuth[3], tempCiph)
+                    if int(checker) == int(initAuth[0]):
+                        logging.info("User validated!")
+                    else:
+                        logging.info("ISSUES! WRONG USER!")
+                    CLIENTS[str(initAuth[0])] = {'IP': initAuth[1], 'PORT': initAuth[2]}
+                    logging.info("Added uesr %s to the connected list with ip: %s, port: %s", str(initAuth[0]),
+                                 str(initAuth[1]), initAuth[2])
+                    conn.sendall(pickle.dumps(CLIENTS))
             else:
                 logging.info("Loaded data from the client: %s", initAuth)
                 sessKey = os.urandom(16)
